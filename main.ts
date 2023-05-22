e7b057b6e8bfa1c4cd7bee558ed0780165e39971
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-05-22 10:58:07 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-basic.showLeds(`
-    . # # . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.forever(function () {
+input.onGesture(Gesture.ThreeG, function () {
 	
+})
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 500) {
+        joystickbit.Vibration_Motor(500)
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 700) {
+        joystickbit.Vibration_Motor(500)
+    }
 })
