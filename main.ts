aa1dfcd3d6e01e0ea56059db80586bd37a7d08a0
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-05-26 16:11:17 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-input.onGesture(Gesture.ThreeG, function () {
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    basic.showString("Boost")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    basic.showString("Accelere")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    basic.showString("frein")
+})
+basic.forever(function () {
 	
 })
 basic.forever(function () {
