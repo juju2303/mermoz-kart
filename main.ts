b7a7d3533ed4955006586155d4cef7bb95f161a5
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-06-01 15:23:46 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-    basic.showString("Boost")
+    radio.sendString("Boost")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    radio.sendString("Decelere")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    basic.showString("Accelere")
+    radio.sendString("Accelere")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-    basic.showString("frein")
+    radio.sendString("Frein")
 })
-radio.setGroup(1)
-basic.forever(function () {
-    radio.sendNumber(joystickbit.getRockerValue(joystickbit.rockerType.X))
-})
+radio.setGroup(11)
 basic.forever(function () {
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 500) {
         joystickbit.Vibration_Motor(500)
