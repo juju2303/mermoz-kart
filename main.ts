03b08f8e21163b40399166ffc1525b3b9af6ed74
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-06-02 16:10:51 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     radio.sendString("Boost")
 })
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("CG")
-})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     radio.sendString("reculer")
 })
 radio.onReceivedString(function (receivedString) {
-    joystickbit.Vibration_Motor(500)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("CD")
+    if (receivedString == "Obstacle") {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        joystickbit.Vibration_Motor(500)
+        basic.clearScreen()
+    }
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radio.sendString("Accelere")
@@ -19,14 +23,16 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     radio.sendString("Frein")
 })
+joystickbit.initJoystickBit()
 radio.setGroup(33)
 radio.setTransmitPower(7)
-joystickbit.initJoystickBit()
 basic.forever(function () {
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 500) {
         radio.sendString("Droite")
+        radio.sendString("CD")
     }
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 700) {
         radio.sendString("Gauche")
+        radio.sendString("CG")
     }
 })
