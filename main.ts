7a8d358fe1b1621ce130d32887926373493823d7
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-06-02 15:14:09 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,43 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-	
+    radio.setGroup(204)
+    radio.setTransmitPower(7)
+    radio.sendString("Boost")
+})
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(23)
+    radio.setTransmitPower(7)
+    radio.sendString("CG")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
-	
+    radio.setGroup(204)
+    radio.setTransmitPower(7)
+    radio.sendString("Decelere")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(23)
+    radio.setTransmitPower(7)
+    radio.sendString("CD")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    basic.showIcon(IconNames.Heart)
+    radio.setGroup(204)
+    radio.setTransmitPower(7)
+    radio.sendString("Accelere")
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-	
+    radio.setGroup(204)
+    radio.setTransmitPower(7)
+    radio.sendString("Frein")
 })
 joystickbit.initJoystickBit()
-radio.setGroup(204)
 basic.forever(function () {
-	
+    radio.setGroup(204)
+    radio.setTransmitPower(7)
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 500) {
+        radio.sendString("Droite")
+    }
+    radio.setGroup(204)
+    radio.setTransmitPower(7)
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 700) {
+        radio.sendString("Gauche")
+    }
 })
