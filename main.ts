61adf79c97f6064db63007ae39c6d0453f869c5e
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-06-05 10:57:27 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,31 @@
 joystickbit.initJoystickBit()
 radio.setGroup(33)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendString("CG_a")
-    } else {
-        radio.sendString("CG_b")
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        radio.sendString("CD_a")
-    } else {
-        radio.sendString("CD_b")
-    }
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 500) {
         radio.sendString("Droite")
     } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 700) {
         radio.sendString("Gauche")
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
         radio.sendString("Accelere")
-    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
-        radio.sendString("reculer")
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
         radio.sendString("Boost")
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
+        radio.sendString("reculer")
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
         radio.sendString("Frein")
+    } else if (false) {
+    	
     } else {
         radio.sendString("NUL")
     }
     joystickbit.initJoystickBit()
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
 })
