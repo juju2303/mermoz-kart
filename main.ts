3c5c27ba76089957ca4171e42a539b81f4b36322
juju2303 <132678792+juju2303@users.noreply.github.com>
2023-06-05 08:56:55 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,23 @@
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
-    radio.sendString("Boost")
-})
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("CG")
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
-    radio.sendString("reculer")
-})
-radio.onReceivedString(function (receivedString) {
-	
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("CD")
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    radio.sendString("Accelere")
-})
-joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
-    radio.sendString("Frein")
-})
 joystickbit.initJoystickBit()
 radio.setGroup(33)
 basic.forever(function () {
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 500) {
         radio.sendString("Droite")
-    }
-    if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 700) {
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 700) {
         radio.sendString("Gauche")
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendString("Accelere")
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P15)) {
+        radio.sendString("reculer")
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendString("Boost")
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendString("Frein")
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendString("CG")
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendString("CD")
+    } else {
+        radio.sendString("NUL")
     }
 })
